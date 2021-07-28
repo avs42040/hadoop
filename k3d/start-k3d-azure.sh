dd505a42c82fab6c0b2aa0f160b2bb70df191807
Suphanat Aviphan <suphanat.aviphan@infologistix.de>
2021-07-28 15:14:04 +0200

    add new backup

--- k3d/start-k3d-azure.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-k3d cluster create --api-port 6550 -p "80:80@loadbalancer" -p "443:443@loadbalancer" --k3s-server-arg="--no-deploy=traefik" --agents 1
-
-cp $(k3d kubeconfig write k3s-default) ~/.kube/config
